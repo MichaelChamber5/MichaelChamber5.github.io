8451c89a28f566ca7d953ad8cbbdcfda2814359f
Michael Chambers <geofreypillagefish@gmail.com>
2025-03-03 17:06:00 -0500

    make some changes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
-import * as THREE from 'https://unpkg.com/three@0.172.0/build/three.module.js';//try this
+import * as THREE from 'three';//try this
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
